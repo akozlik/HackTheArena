727877f1b34bd122fe5685b277984c5526de6a7c
Andrew Kozlik <akozlik@unikey.com>
2016-02-27 21:31:48 -0500

    Working on styling

--- app/express/routes/index.js
+++ app/express/routes/index.js
@@ -4,26 +4,30 @@ var request = require('request');
 var api = require('../../api');
 
 router.get('/', function(req, res) {
-	console.log(req, res);
-	console.log("Hullo werld");
 	res.render('pages/index', {
-
+		title: "Log In To Win"
 	});
 });
 
 router.get('/seats', function(req, res) {
 	console.log("Seats");
-	res.render('pages/seats', {});
+	res.render('pages/seats', {
+		title: "Where are you sitting?"
+	});
 });
 
 router.get('/engagement', function(req, res) {
 	console.log("Engagement");
-	res.render('pages/engagement', {});
+	res.render('pages/engagement', {
+		title: "Play to Win!"
+	});
 });
 
 router.get('/register', function(req, res) {
 	console.log("Registration");
-	res.render('pages/register', {});
+	res.render('pages/register', {
+		title: "Register"
+	});
 });
 
 var clientsController = require('../../api/clients');
