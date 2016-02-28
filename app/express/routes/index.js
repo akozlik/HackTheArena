47ddba649af33567d78d8cda242db41ef6863077
Andrew Kozlik <akozlik@unikey.com>
2016-02-28 13:44:43 -0500

    Trying to fix facebook callback

--- app/express/routes/index.js
+++ app/express/routes/index.js
@@ -34,11 +34,10 @@ router.get('/results', function(req, res) {
 router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
 
 router.get('/auth/facebook/callback',
-	passport.authenticate('facebook', {
-		successRedirect : '/seats',
-		failureRedirect : '/'
-	})
-);
+	passport.authenticate('facebook', {failureRedirect: '/' }),
+	function(req, res) {
+		res.redirect('/seats');
+});
 
 router.get('/register', function(req, res) {
 	console.log("Registration");
